19b4d3db1a70740deecab7d2fc70125de8c40b5a
CodeNinja-tech <elo48858@gmail.com>
2023-01-31 14:46:20 +0100

    `🌱INIT:` Created Project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val rockPlanets = arrayOf<String>(
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars"
+    )
+
+    val gasPlanets = arrayOf<String>(
+        "Jupiter",
+        "Saturn",
+        "Uranus",
+        "Neptune"
+    )
+
+    val solarSystem = rockPlanets + gasPlanets
+
+    var i = 0
+
+    repeat(solarSystem.size) {
+        println(solarSystem[i])
+        i ++
+    }
+}
